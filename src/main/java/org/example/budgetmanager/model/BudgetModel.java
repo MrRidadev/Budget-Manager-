ef639670cf2511da887c8ce6085f9dbff79cfff2
youssfmadani <youssfelmadani@gmail.com>
2025-05-13 11:43:34 +0100

    Ajout model Budget

--- src/main/java/org/example/budgetmanager/model/BudgetModel.java
+++ src/main/java/org/example/budgetmanager/model/BudgetModel.java
@@ -1,4 +1,20 @@
 package org.example.budgetmanager.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BudgetModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String category;
+    private Double limitAmount;
+    private Double spentAmount;
+
 }
